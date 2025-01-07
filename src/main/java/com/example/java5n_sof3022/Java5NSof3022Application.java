09fd308fb2ee31902ba5f45288952ca09cabcdcd
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-01-07 09:46:41 +0700

    Set up default spring boot MVC; customizing console logout messages in  application.properties file

---
+++ src/main/java/com/example/java5n_sof3022/Java5NSof3022Application.java
@@ -0,0 +1,14 @@
+package com.example.java5n_sof3022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java5NSof3022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java5NSof3022Application.class, args);
+        System.out.println("running...");
+    }
+
+}
---
+++ src/test/java/com/example/java5n_sof3022/Java5NSof3022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java5n_sof3022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java5NSof3022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
