d6d7bf5f96978fba5c6182667d1995beaa1e4b35
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-01-10 07:11:32 +0700

    Adding Bootstrap

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,4 +4,5 @@ SET IDENTITY_INSERT students ON;
 INSERT INTO students (id, name, email, phone) VALUES (1001, 'Student 1', 'Email 1', 'Phone 1');
 INSERT INTO students (id, name, email, phone) VALUES (1002, 'Student 2', 'Email 2', 'Phone 2');
 INSERT INTO students (id, name, email, phone) VALUES (1003, 'Student 3', 'Email 3', 'Phone 3');
+INSERT INTO students (id, name, email, phone) VALUES (1004, 'Student 4', 'Email 4', 'Phone 4');
 SET IDENTITY_INSERT students OFF;
